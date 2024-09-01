bd28e0f948a62dfaa05f90e8968b97f698d88237
spaciousejar <jadhavvicky752@gmail.com>
2024-09-02 02:59:15 +0530

    tttt

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -6,9 +6,9 @@ export async function SiteFooter() {
   return (
     <footer className="py-6 md:px-8 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-        <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
-        <p className="text-sm">&copy; {new Date().getFullYear()} Xlbrain, Inc.</p>
-  <a href="https://github.com/spaciousejar" className="inline-block ml-2 hover:text-gray-400">GitHub</a>
+        <p className="text-balance text-center text-sm leading-loose text-muted-foreground ">
+        &copy; {new Date().getFullYear()} Xlbrain, Inc.
+  <a href="" className="inline-block ml-2">GitHub</a>
   <a href="#" className="inline-block ml-2">X</a>
 
         </p>
